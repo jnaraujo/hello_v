27027de1a507a031c14a66d087675b6b308cb335
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-07 17:03:11 -0300

    test

--- src/main.v
+++ src/main.v
@@ -1,6 +1,26 @@
 module main
 
-import os
+import net.http
+import sync
+import time
+
+fn task(mut wg sync.WaitGroup, id int) {
+	start := time.ticks()
+
+	http.get("https://google.com") or {}
+
+	println("Task ${id} took ${time.ticks() - start} ms")
+	wg.done()
+}
 
 fn main() {
+
+	mut wg := sync.new_waitgroup()
+	wg.add(16)
+
+	for i in 0 .. 15 {
+		go task(mut wg, i)
+	}
+
+	wg.wait()
 }
